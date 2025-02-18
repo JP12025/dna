9872db01bdf33010cb4962d01b23d29501815c81
Guillaume Collet <bilouweb@free.fr>
2025-02-18 10:03:25 +0100

    update project

---
+++ dna.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+
+from matcher import longest_match
+
+if __name__ == "__main__":
+
+    # TODO: Check for command-line usage
+
+    # TODO: Read database file into a variable
+
+    # TODO: Read DNA sequence file into a variable
+
+    # TODO: Find longest match of each STR in DNA sequence
+
+    # TODO: Check database for matching profiles
+
+    pass
